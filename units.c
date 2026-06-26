670c3ce49f2bb78c9da5f6979e852a2dd5882894
ezra522 <ezranyaosi522@gmail.com>
2026-06-26 08:55:52 +0300

    Create units.c to display student info and units
    Added a program to display student details and units offered this semester.

---
+++ units.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+
+    // Student details
+    printf("Name: Ezra Nyaosi\n");
+    printf("Registration Number: BDSA-03-0029/2026\n");
+    printf("Course: Bachelor of Data Science and Analytics\n\n");
+
+    // Units offered
+    printf("Units Offered This Semester:\n\n");
+
+    printf("BCU 111: COMMUNICATION SKILLS\n");
+    printf("Core\n\n");
+
+    printf("BCU 112: DIGITAL LITERACY\n");
+    printf("Core\n\n");
+
+    printf("BCU 113: HEALTH AND WELLNESS MANAGEMENT\n");
+    printf("Core\n\n");
+
+    printf("BDA 111: DATA ETHICS, PRIVACY AND GOVERNANCE\n");
+    printf("Core\n\n");
+
+    printf("BSD 111: STRUCTURED PROGRAMMING AND ALGORITHMS\n");
+    printf("Core\n\n");
+
+    printf("MAT 111: BASIC MATHEMATICS\n");
+    printf("Core\n");
+
+    return 0;
+}
